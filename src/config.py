f7f0b0e17f5e3570a9b86270e8adf2d421cc0f55
mk1381 <mk1381@rit.edu>
2022-05-16 16:40:52 -0400

    Updated config.py to fix bug.

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import torch
 
 #Device configuration
-DEVICE = torch.device('cuda') if torch.cuda.is_available else torch.device('cpu')
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 #ML Parameters
 BATCH_SIZE = 4
