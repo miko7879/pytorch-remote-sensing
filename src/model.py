ccc28ba70ee5d7ca12ef6463fb66b0281bae58fc
mk1381 <mk1381@rit.edu>
2022-05-17 13:50:40 -0400

    Changed model to use retinanet backbone.

--- src/model.py
+++ src/model.py
@@ -5,7 +5,7 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 def create_model(num_classes):
 
     #Load the pre-trained model
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained = True)
+    model = torchvision.models.detection.retinanet_resnet50_fpn(pretrained = True)
     
     #Get the number of input features to the original box predictor
     in_features = model.roi_heads.box_predictor.cls_score.in_features
